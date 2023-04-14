6e27f4f521f09e768bb8f6023c4c107f3ee03046
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2023-04-14 06:31:58 +0000

    added the main.cpp with mainmenu for the project

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void mainmenu();
+int main()
+{
+
+}
+void mainmenu()
+{
+    cout<<"___________________________________________________________________\n";
+    cout<<"\t\t\tSelect an option to proceed.\n";
+    cout<<"\t  1- Set Current User.\n";
+    cout<<"\t  2- View home of current user.\n";
+    cout<<"\t  3- Like a post.\n";
+    cout<<"\t  4- View list of people who liked a post.\n";
+    cout<<"\t  5- Comment on a post.\n";
+    cout<<"\t  6- View post.\n";
+    cout<<"\t  7- Share post.\n";
+    cout<<"\t  8- View user's profile.\n";
+    cout<<"\t  9- View connections.\n";
+    cout<<"\t  10- View job postings.\n";
+    cout<<"\t  11- Search.\n";
+    cout<<"________________________________________________________________\n";
+}
